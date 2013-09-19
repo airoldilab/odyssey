717c1abb6e72a0ff63d41650c9d95556209886a8
Gabor Csardi <gcsardi@stat.harvard.edu>
2013-09-19 09:25:59 -0400

    R package: added a function to run all tests.

--- R221/R/util.R
+++ R221/R/util.R
@@ -4,3 +4,9 @@ fracSec <- function() {
   as.numeric(format(Sys.time(), "%OS")) * 10^5
 }
 
+## Run all tests of the package
+runTests <- function() {
+  require(testthat)
+  tdir <- system.file("tests", package="R221")
+  test_dir(tdir)
+}
